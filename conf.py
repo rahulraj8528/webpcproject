96a8ec95307eb24db2405afd0b1e9c6f51e06135
rahulraj8528 <rahulraj8528@outlook.com>
2025-09-24 23:43:07 +0530

    Update conf.py

--- conf.py
+++ conf.py
@@ -29,8 +29,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 
 html_theme = 'alabaster'
-html_title = "THIS IS MY CUSTOM TITLE"
-project = "MY PROJECT NAME"
+html_title = "Download and install vipre using product key for maximum protection"
 
 # You can still add html_meta here if you want, but with the new Read the Docs addons
 # custom template is the reliable way to inject meta tags.
